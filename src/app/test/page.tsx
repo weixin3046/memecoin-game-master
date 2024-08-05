239c8251946d02dfa5dd61f97e5af9f62384d3ca
米润中 <mirz@fortunet.com.hk>
2024-08-05 15:11:55 +0800

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,5 +4,5 @@ import { SignIn } from "@/components/LoginForm/auth-components";
 export default async function TestPage() {
   const session = await auth();
   if (!session?.user) return <SignIn />;
-  return <div>{session.user.email}</div>;
+  return <div>{session.user.email} test</div>;
 }
